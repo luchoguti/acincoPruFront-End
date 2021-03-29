2c84d5083dd80a0e6fb61ff09c8982cdccf27f97
Luis Alberto Torres Gutierrez <luchoguti9@gmail.com>
2021-03-29 14:14:27 -0500

    se cambiar url del api

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 //export const API_BASE_URL = 'http://127.0.0.1:8000/api';
-export const API_BASE_URL = 'https://dudley-loon-67593.herokuapp.com/api';
+export const API_BASE_URL = 'https://acincoprubackend.herokuapp.com/api';
